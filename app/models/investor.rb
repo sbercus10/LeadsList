53b37f231e05c821c54c2fca386db5f11d91aa88
Adam Hennings <hennings.adam@gmail.com>
2014-03-01 15:22:37 -0700

    prior to rearranging company and investor relationships

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,5 +1,5 @@
 class Investor < ActiveRecord::Base
-  attr_accessible :angellist_image_url, :angellist_url, :bio, :company_name, :company_url, :email_address, :follower_count, :investment_status, :job_title, :location, :name, :phone_number
+  attr_accessible :angellist_image_url, :angellist_url, :bio, :company_name, :company_url, :email_address, :follower_count, :investment_status, :job_title, :location, :name, :phone_number, :company_id
 
   has_and_belongs_to_many :companies
 end
